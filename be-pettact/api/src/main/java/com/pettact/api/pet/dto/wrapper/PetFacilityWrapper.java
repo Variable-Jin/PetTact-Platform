ed6732479793251b2af3e1665b292d13a09f4ec2
ji4ang <keh3056@naver.com>
2025-06-20 20:57:33 +0900

    wrapper 불필요한 어노테이션 제거 - 김지상

--- be-pettact/api/src/main/java/com/pettact/api/pet/dto/wrapper/PetFacilityWrapper.java
+++ be-pettact/api/src/main/java/com/pettact/api/pet/dto/wrapper/PetFacilityWrapper.java
@@ -1,6 +1,8 @@
-package com.pettact.api.pet.dto;
+package com.pettact.api.pet.dto.wrapper;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.pettact.api.pet.dto.PetFacilityDto;
+
 import lombok.Data;
 
 import java.util.List;
@@ -8,21 +10,11 @@ import java.util.List;
 @Data
 public class PetFacilityWrapper {
 
-    @JsonProperty("data")
     private List<PetFacilityDto> data;
 
-    @JsonProperty("currentCount")
     private int currentCount;
-
-    @JsonProperty("matchCount")
     private int matchCount;
-
-    @JsonProperty("page")
     private int page;
-
-    @JsonProperty("perPage")
     private int perPage;
-
-    @JsonProperty("totalCount")
     private int totalCount;
 }
