c857078bf2a017275bb67bf45ff0889f5f886993
YONGMINJO1 <iy0594@naver.com>
2025-06-27 13:22:47 +0900

    Cart CRUD ( User 연동 완료 ) 기능 구현 - 조용민

--- be-pettact/api/src/main/java/com/pettact/api/cart/repository/CartRepository.java
+++ be-pettact/api/src/main/java/com/pettact/api/cart/repository/CartRepository.java
@@ -8,20 +8,19 @@ import org.springframework.stereotype.Repository;
 
 import com.pettact.api.cart.entity.CartEntity;
 import com.pettact.api.product.entity.ProductEntity;
+import com.pettact.api.user.entity.Users;
 
 @Repository
 public interface CartRepository extends JpaRepository<CartEntity, Long> {
 	
-    List<CartEntity> findByUserId(Long userId);
+    // 해당 유저의 모든 장바구니 목록 조회
+	List<CartEntity> findByUser_UserNo(Long userNo);
 
-    Optional<CartEntity> findByUserIdAndProduct(Long userId, ProductEntity product);
 
-//    // 해당 유저의 모든 장바구니 목록 조회
-//    List<CartProductEntity> findByUser(UserEntity user);
-//
-//    // 유저와 상품으로 장바구니 아이템 존재 여부 확인
-//    Optional<CartProductEntity> findByUserAndProduct(UserEntity user, ProductEntity product);
-//
-//    // 유저의 장바구니에서 특정 상품 제거
-//    void deleteByUserAndProduct(UserEntity user, ProductEntity product);
+    // 유저와 상품으로 장바구니 아이템 존재 여부 확인
+    Optional<CartEntity> findByUserAndProduct(Users userNo, ProductEntity product);
+
+    // 유저의 장바구니에서 특정 상품 제거
+    void deleteByUserAndProduct(Users user, ProductEntity product);
+   
 }
